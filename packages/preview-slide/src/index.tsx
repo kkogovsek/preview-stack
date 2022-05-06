5b6db1df672c3c820507e0f121b0177b8be870dd
Domen Gabrovsek <domen.gabrovsek@1337.tech>
2022-05-06 13:15:23 +0200

    woohoo

--- packages/preview-slide/src/index.tsx
+++ packages/preview-slide/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import tw from "tw";
 
 export default function Slide() {
-  return <Test>We did it in production! One more time!</Test>;
+  return <Test>We did it in production! One more time! And one more time!</Test>;
 }
 
 const Test = tw.div`bg-gray-800 text-white flex items-center justify-center p-2 text-xl border-pink-100 border-dotted`;
