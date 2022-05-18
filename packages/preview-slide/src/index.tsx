8a7092451cfe738649012ce6b390f54973d39970
Klemen Kogovšek <klemen.kogovsek@tretton37.com>
2022-05-18 20:53:44 +0200

    Test

--- packages/preview-slide/src/index.tsx
+++ packages/preview-slide/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import tw from "tw";
 
 export default function Slide() {
-  return <Test>We did it</Test>;
+  return <Test>Hello 👋</Test>;
 }
 
-const Test = tw.div`bg-gray-800 text-white flex items-center justify-center p-2 text-xl border-pink-100 border-dotted`;
+const Test = tw.div`bg-red-500 text-white flex items-center justify-center p-2 text-xl border-pink-100 border-dotted`;
